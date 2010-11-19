fb6d08c6353630f020be2bd11fff63c42dfdee4e
Jason Mobarak <github@d43d4l.us>
2010-11-18 21:14:58 -0700

    Add script to copy files for extension gallery.

---
+++ make_pkg.sh
@@ -0,0 +1,15 @@
+#!/bin/sh
+
+rm -rf pkg
+mkdir pkg
+cp background.html pkg
+cp blip_cs_acty.js pkg
+cp icon-128.png pkg
+cp icon-48.png pkg
+cp jquery-1.4.1.min.js pkg
+cp jquery-ui-1.8.5.custom.css pkg
+cp jquery-ui-1.8.5.custom.min.js pkg
+cp jquery-watch-plugin-1.2.0.js pkg
+cp manifest.json pkg
+cp options.html pkg
+cp options.js pkg
