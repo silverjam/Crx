a118f7a936354192ddb9ae7567ae194654c4369c
Jason Mobarak <x@jason.mobarak.name>
2011-08-06 01:44:54 -0600

    Implement options portion of changing the icon type.

--- GplusOneUp/make_pkg.py
+++ GplusOneUp/make_pkg.py
@@ -32,11 +32,24 @@ cp("oneup.css", pkg)
 
 cp("icon-15.png", pkg)
 cp("icon-15-inv.png", pkg)
+
+cp("icon-15g.png", pkg)
+cp("icon-15g-inv.png", pkg)
+
+cp("icon-15g1.png", pkg)
+cp("icon-15g1-inv.png", pkg)
+
+cp("icon-15r.png", pkg)
+cp("icon-15r-inv.png", pkg)
+
 cp("icon-16.png", pkg)
 cp("icon-48.png", pkg)
 cp("icon-64.png", pkg)
 cp("icon-128.png", pkg)
 cp("icon-256.png", pkg)
+
+cp("1up-anlg.mp3", pkg)
+cp("1up-dig.mp3", pkg)
 
 chdir(pkg)
 
