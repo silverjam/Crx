8f4eeff06744230a325b469a6fe5a5fda7329f7e
Jason Mobarak <x@jason.mobarak.name>
2012-01-07 03:22:51 -0700

    Change extension IDs for chrome webstore.

--- GplusOneUp/sedextid.py
+++ GplusOneUp/sedextid.py
@@ -4,12 +4,15 @@ import sys
 
 for root, dirs, files in os.walk('.'):
 
+	if root != '.':
+		continue
+
 	for filename in files:
 
 		if (filename.endswith(".css")  or 
 			filename.endswith(".html") or 
 			filename.endswith(".js")):
-			
+
 			fp = open(filename)
 
 			data = fp.read()
